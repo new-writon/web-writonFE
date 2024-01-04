9213d448f0b4a9db7d9b3df5c24637fb23d7dd2b
hoyyChoi <110888511+hoyyChoi@users.noreply.github.com>
2024-01-04 23:14:50 +0900

    feat : routing

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,21 +1,36 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Layout from "@/Layout";
-import MainPage from "@/pages/MainPage";
+import LoginPage from "@/pages/Authorization/LoginPage";
+import RegisterEmailPage from "@/pages/Authorization/RegisterEmailPage";
+import RegisterPage from "@/pages/Authorization/RegisterPage";
+import MainPage from "@/pages/MainPage/MainPage";
 
 const router = () => {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route element={<Layout />}>
-					<Route
-						path="/"
-						element={<MainPage />}
-					/>
-				</Route>
-			</Routes>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/login"
+          element={<LoginPage />}
+        />
+        <Route
+          path="/register"
+          element={<RegisterPage />}
+        />
+        <Route
+          path="/registeremail"
+          element={<RegisterEmailPage />}
+        />
+        <Route element={<Layout />}>
+          <Route
+            path="/"
+            element={<MainPage />}
+          />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default router;
