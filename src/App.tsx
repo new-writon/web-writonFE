b1f61240d4e2341272e5e122861edec4ed48028f
hoyyChoi <110888511+hoyyChoi@users.noreply.github.com>
2024-01-04 23:08:49 +0900

    chore : indent

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,12 @@ import { RecoilRoot } from "recoil";
 import Router from "./router";
 import { GlobalStyle } from "./style/global";
 const App = () => {
-	return (
-		<RecoilRoot>
-			<GlobalStyle />
-			<Router />
-		</RecoilRoot>
-	);
+  return (
+    <RecoilRoot>
+      <GlobalStyle />
+      <Router />
+    </RecoilRoot>
+  );
 };
 
 export default App;
