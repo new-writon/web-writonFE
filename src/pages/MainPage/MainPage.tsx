924cb3236685a9a19d8b9fbca51954153fee6a02
hoyyChoi <110888511+hoyyChoi@users.noreply.github.com>
2024-01-05 02:18:59 +0900

    feat : progressBar

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,7 +1,25 @@
+import { useEffect, useState } from "react";
+
+import { AnimatedLineProgressBar } from "@frogress/line";
 import styled from "styled-components";
 
 const MainPage = () => {
-  return <Container>MainPage</Container>;
+  const [value, setValue] = useState(0);
+  useEffect(() => {
+    setTimeout(() => {
+      setValue(70);
+    }, 700);
+  }, []);
+  return (
+    <Container>
+      <AnimatedLineProgressBar
+        percent={value}
+        rounded={36}
+        height={36}
+        transition={{ easing: "easeInOut" }}
+      />
+    </Container>
+  );
 };
 
 export default MainPage;
