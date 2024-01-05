6c405e0badf856970885667397cad8c2fccea0ee
hoyyChoi <110888511+hoyyChoi@users.noreply.github.com>
2024-01-06 04:48:20 +0900

    feat : MainSemiTitle component

---
+++ src/components/atom/MainSemiTitle/index.tsx
@@ -0,0 +1,7 @@
+import { ChildrenProps } from "@/types";
+
+import { Container } from "./style";
+
+export const MainSemiTitle = ({ children }: ChildrenProps) => {
+  return <Container>{children}</Container>;
+};
---
+++ src/components/atom/MainSemiTitle/style.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  color: var(--Gray-100, #212121);
+  font-size: 1.25rem;
+  font-weight: 700;
+  margin: auto 0;
+  line-height: 22px;
+`;
