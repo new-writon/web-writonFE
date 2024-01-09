6548db2b6b8728bb23deff7c4232fa5cc90d1509
hoyyChoi <110888511+hoyyChoi@users.noreply.github.com>
2024-01-10 01:19:14 +0900

    feat : add floatingBtn

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,15 +1,21 @@
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
 import { Calendar } from "@/components/MainPage/Calendar";
 import { MyRetrospect } from "@/components/MainPage/MyRetrospect";
 import { ProgressBox } from "@/components/MainPage/ProgressBox";
+import { FloatingWriteButton } from "@/components/atom/button";
 
 const MainPage = () => {
+  const navigate = useNavigate();
   return (
     <Container>
       <ProgressBox />
       <Calendar />
       <MyRetrospect />
+      <FloatingWriteButton onClick={() => navigate("/writepage")}>
+        회고 작성하기
+      </FloatingWriteButton>
     </Container>
   );
 };
@@ -19,4 +25,5 @@ export default MainPage;
 const Container = styled.div`
   background: var(--Gray2_100, #f5f5f5);
   padding-top: 23px;
+  position: relative;
 `;
