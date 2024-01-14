ca6b04adfe91c0454e08eeb95bd370b3661dea79
hoyyChoi <110888511+hoyyChoi@users.noreply.github.com>
2024-01-15 01:30:23 +0900

    feat : communityBox component

--- src/components/CommunityPage/CommunityBox/index.tsx
+++ src/components/CommunityPage/CommunityBox/index.tsx
@@ -1,5 +1,73 @@
-import { Container } from "./style";
+import React, { useEffect, useState } from "react";
+
+import { format } from "date-fns";
+
+import arrow from "@/assets/communityPage/storyArrow.svg";
+import { CommunityItem } from "@/components/atom/CommunityItem";
+import { MainSemiTitle } from "@/components/atom/MainSemiTitle";
+import { TitleSideBox } from "@/components/atom/TitleSideBox";
+import { communityDummy } from "@/dummy/community";
+import { Inner } from "@/style/global";
+
+import { CommunityHeader, CommunityItemBox, Container } from "./style";
 
 export const CommunityBox = () => {
-  return <Container></Container>;
+  const today = new Date();
+  const [width, setWidth] = useState<number>(window.innerWidth);
+
+  const handleResize = () => {
+    //뷰크기 강제로 강져오기
+    setWidth(window.innerWidth);
+  };
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize); //clean
+  }, [width]);
+
+  return (
+    <Inner>
+      <Container>
+        <CommunityHeader>
+          <div className="title">
+            {width > 560 ? (
+              <>
+                <MainSemiTitle font={1.25}>
+                  {format(today, "M")}월 {format(today, "d")}일, <div className="number">{23}</div>
+                  명이 챌린지를 완료했어요.
+                </MainSemiTitle>
+                <TitleSideBox type="default">오늘</TitleSideBox>
+              </>
+            ) : (
+              <>
+                <MainSemiTitle font={1.125}>
+                  {format(today, "d")}일,<div className="number second">{23}</div>명이 챌린지를
+                  완료했어요.
+                </MainSemiTitle>
+              </>
+            )}
+          </div>
+          <div className="changeDate">
+            <img
+              className="previous"
+              src={arrow}
+              alt="<"
+            />
+            <div className="today">오늘</div>
+            <img
+              className="next"
+              src={arrow}
+              alt="<"
+            />
+          </div>
+        </CommunityHeader>
+        <CommunityItemBox>
+          {communityDummy.map((item, idx) => (
+            <React.Fragment key={idx}>
+              <CommunityItem data={item} />
+            </React.Fragment>
+          ))}
+        </CommunityItemBox>
+      </Container>
+    </Inner>
+  );
 };
--- src/components/CommunityPage/CommunityBox/style.ts
+++ src/components/CommunityPage/CommunityBox/style.ts
@@ -1,3 +1,59 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  @media (max-width: 530px) {
+    background-color: var(--Gray-20, #f5f5f5);
+  }
+`;
+
+export const CommunityHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 31px 0 21px;
+  position: -webkit-sticky;
+  position: sticky;
+  top: 71px;
+  z-index: 9999;
+  border-bottom: 1px solid var(--Gray-30);
+  background-color: var(--Gray-20, #f5f5f5);
+  padding-left: 26px;
+  @media (max-width: 530px) {
+    top: 107px;
+    padding-left: 0;
+  }
+  .title {
+    display: flex;
+    gap: 6px;
+  }
+  .changeDate {
+    display: flex;
+    align-items: center;
+  }
+  .changeDate .today {
+    color: var(--Gray-80, #616161);
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 22px;
+    cursor: pointer;
+    margin: 0 8px;
+  }
+  .changeDate .previous {
+    cursor: pointer;
+  }
+
+  .changeDate .next {
+    cursor: pointer;
+    transform: rotate(180deg);
+  }
+`;
+
+export const CommunityItemBox = styled.div`
+  grid-template-columns: 1fr 1fr;
+  display: grid;
+  gap: 18px;
+  padding-bottom: 150px;
+  @media (max-width: 840px) {
+    grid-template-columns: 1fr;
+  }
+`;
