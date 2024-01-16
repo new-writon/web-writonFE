18250f44704bfe4be2f14d0ffadc781e2f869855
hoyyChoi <110888511+hoyyChoi@users.noreply.github.com>
2024-01-17 00:26:14 +0900

    feat : login api

--- src/apis/login.ts
+++ src/apis/login.ts
@@ -1,57 +1,77 @@
-import { WRITON, getData, postData } from ".";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { UserInfo, kakaoType } from "@/types/user";
 
-export const postLogin = async ({ id, pw }: { id: string; pw: string }) => {
-  const res = await WRITON.post("/user/login", {
-    userIdentifier: id,
-    userPassword: pw,
+import { getData, postData } from ".";
+
+// 로그인
+export const postLogin = async ({
+  id,
+  pw,
+  organization,
+}: {
+  id: string;
+  pw: string;
+  organization: string;
+}) => {
+  const response = await postData<UserInfo>("/auth/login/local", {
+    identifier: id,
+    password: pw,
+    organization: organization,
   });
-  return res;
-};
-interface GetDuplicateId {
-  code: number;
-  message: string;
-}
-export const getDuplicateId = async (userId: string) => {
-  const response = await getData<GetDuplicateId>(
-    `/user/check-identifier?checkIdentifier=${userId}`
-  );
-  return response;
-};
-
-export const postEmail = async (email: string) => {
-  const response = await postData("/user/signup/verify-email-code", { email });
-  return response;
-};
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const postEmailCode = async ({ email, emailCode }: any) => {
-  const response = await postData("/user/signup/verify-email", {
-    email: email,
-    code: emailCode,
-  });
-  return response;
+  return response.data;
 };
 
 export const postKakaoAuth = async (CODE: string) => {
-  const response = await postData(
+  const response = await postData<kakaoType>(
     `https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id=${
       import.meta.env.VITE_APP_REST_API_KEY
     }&redirect_uri=${import.meta.env.VITE_APP_REDIRECT_URI}&code=${CODE}`,
     {}
   );
+  return response.data;
+};
+
+export const postKakaoLogin = async (token: string, organization: string) => {
+  const response = await postData<UserInfo>(
+    "/auth/login/kakao",
+    { organization: organization },
+    {
+      headers: {
+        Authentication: token,
+      },
+    }
+  );
+  return response.data;
+};
+
+//회원가입
+export const getDuplicateId = async (userId: string) => {
+  const response = await getData(`/user/identifier/check?identifier=${userId}`);
+  return response.data;
+};
+
+export const getDuplicateEmail = async (email: string) => {
+  const response = await getData(`/user/email/check?email=${email}`);
+  return response;
+};
+export const postEmail = async (email: string) => {
+  const response = await postData<{ code: number }>("/auth/generate/email-code", { email });
   return response;
 };
 
-export const postKakaoLogin = async (token: string) => {
-  const response = await postData(
-    "/user/kakao-login",
-    {
-      headers: {
-        "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
-        access: token,
-      },
-    },
-    {}
-  );
-  return response;
+export const postEmailCode = async (email: string, emailCode: string) => {
+  const response = await postData("/auth/verify/email-code", {
+    email: email,
+    code: emailCode,
+  });
+  return response.data;
+};
+
+export const postRegister = async (id: string, pw: string, email: string) => {
+  const response = await postData("/auth/signup", {
+    identifier: id,
+    password: pw,
+    email: email,
+  });
+  return response.data;
 };
