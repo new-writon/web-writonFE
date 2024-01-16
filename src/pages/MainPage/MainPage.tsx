48ed1455ad7b6cf06ee88dfe099fb65d3adc983f
hoyyChoi <110888511+hoyyChoi@users.noreply.github.com>
2024-01-17 00:30:51 +0900

    feat : mainPage queystring

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,21 +1,43 @@
+import { useEffect, useState } from "react";
+
 import { format } from "date-fns";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
+import { getChallengeCurrent } from "@/apis/mainPage";
 import { Calendar } from "@/components/MainPage/Calendar";
 import { MyRetrospect } from "@/components/MainPage/MyRetrospect";
 import { ProgressBox } from "@/components/MainPage/ProgressBox";
 import { FloatingWriteButton } from "@/components/atom/button";
+import { ChallengeCurrentType } from "@/types";
 const MainPage = () => {
   const navigate = useNavigate();
   const today = format(new Date(), "yyyy-MM-dd");
+  const [ChallengeCurrent, setChallengeCurrent] = useState<ChallengeCurrentType>();
   const spaceToWritingPage = () => {
     const date = encodeURI(encodeURIComponent(today));
     navigate(`/writing/${date}`);
   };
+
+  const mainPageRendering = async () => {
+    try {
+      const response = await getChallengeCurrent(
+        localStorage.getItem("organization") || "",
+        localStorage.getItem("challengeId") || "1"
+      );
+      console.log(response);
+      setChallengeCurrent(response);
+    } catch {
+      throw new Error("shit");
+    }
+  };
+
+  useEffect(() => {
+    mainPageRendering();
+  }, []);
   return (
     <Container>
-      <ProgressBox />
+      <ProgressBox ChallengeCurrent={ChallengeCurrent} />
       <Calendar />
       <MyRetrospect />
       <FloatingWriteButton onClick={spaceToWritingPage}>
