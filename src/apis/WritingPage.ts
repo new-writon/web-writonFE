65e92933d17facc348e21da81d8894127aa04bc6
hoyyChoi <110888511+hoyyChoi@users.noreply.github.com>
2024-01-18 19:32:19 +0900

    feat : postWriting API

--- src/apis/WritingPage.ts
+++ src/apis/WritingPage.ts
@@ -1,6 +1,6 @@
-import { BasicQuestionType, SpecialQuestionType } from "@/types";
+import { BasicQuestionType, SpecialQuestionType, postWritingDataType } from "@/types";
 
-import { getData } from ".";
+import { getData, postData } from ".";
 
 // 글쓰기 페이지 베이직 질문 조희
 export const getBasicQuestion = async (challengeId: string) => {
@@ -15,5 +15,21 @@ export const getSpecialQuestion = async (challengeId: string) => {
   const response = await getData<SpecialQuestionType[]>(
     `/challenge/write/${challengeId}/special-question`
   );
+  return response.data;
+};
+
+// 글쓰기 submit 저장
+export const postwritingSubmit = async (
+  organization: string,
+  challengeId: string,
+  date: string,
+  templateContent: postWritingDataType[]
+) => {
+  const response = await postData("challenge/write", {
+    organization,
+    challengeId,
+    date,
+    templateContent,
+  });
   return response.data;
 };
