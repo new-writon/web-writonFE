2d496101ce2008b79292a4a313f4ba607f48df73
hoyyChoi <110888511+hoyyChoi@users.noreply.github.com>
2024-01-18 17:41:55 +0900

    feat : retrospectItem connect API

--- src/components/atom/RetrospectItem/index.tsx
+++ src/components/atom/RetrospectItem/index.tsx
@@ -1,18 +1,24 @@
-import { RetrospectItemProps } from "@/types";
+import { format } from "date-fns";
+import { ko } from "date-fns/locale";
+
+import { RetrospectCurrentType } from "@/types";
 
 import { TitleSideBox } from "../TitleSideBox";
 
 import { Container, PreviewBody, Title } from "./style";
 
-export const RetrospectItem = ({ data }: { data: RetrospectItemProps }) => {
+export const RetrospectItem = ({ data }: { data: RetrospectCurrentType[] }) => {
+  const arr = data?.filter((item) => item.category === "스페셜 질문");
   return (
     <Container>
       <Title>
-        <div className="mainTitle">{data?.date}</div>
-        {data?.special && <TitleSideBox type="special">스페셜 질문</TitleSideBox>}
+        <div className="mainTitle">
+          {format(data[0].finished_at, "M월 d일 EEE요일", { locale: ko })}
+        </div>
+        {arr.length && <TitleSideBox type="special">스페셜 질문</TitleSideBox>}
       </Title>
       <PreviewBody>
-        {data?.preview.map((item, idx) => {
+        {data?.map((item, idx) => {
           return (
             <div
               className="previewItem"
