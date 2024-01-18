3ad96fe9548b428771ffb9d3d80360543f90238a
hoyyChoi <110888511+hoyyChoi@users.noreply.github.com>
2024-01-18 17:45:32 +0900

    feat : add recoil atom

--- src/recoil/atoms/index.ts
+++ src/recoil/atoms/index.ts
@@ -1,5 +1,7 @@
 import { atom } from "recoil";
 
+import { BasicQuestionType, SpecialQuestionType, addSpecialQuestionArrayType } from "@/types";
+
 //common
 export const loadingState = atom({
   key: "loadingState",
@@ -10,4 +12,28 @@ export const loadingState = atom({
 export const agreeTextState = atom({
   key: "agreeTextState",
   default: -1,
+});
+
+//글쓰기 베이직 질문 배열
+export const getBasicQuestionState = atom<BasicQuestionType[]>({
+  key: "getBasicQuestionState",
+  default: [],
+});
+
+//글쓰기 베이직 질문 배열
+export const getSpecialQuestionState = atom<SpecialQuestionType[]>({
+  key: "getSpecialQuestionState",
+  default: [],
+});
+
+// 스페셜 질문 추가 버튼(click)
+export const addSpecialQuestionState = atom<number[]>({
+  key: "addSpecialQuestionState",
+  default: [],
+});
+
+// 스페셜 질문 추가 배열
+export const addSpecialQuestionArrayState = atom<addSpecialQuestionArrayType[]>({
+  key: "addSpecialQuestionArrayState",
+  default: [],
 });
