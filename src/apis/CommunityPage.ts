3c799fb3f58a80a3e0e955be6c90a79882c8df75
hoyyChoi <110888511+hoyyChoi@users.noreply.github.com>
2024-01-24 00:51:00 +0900

    feat : communityPage API

---
+++ src/apis/CommunityPage.ts
@@ -0,0 +1,55 @@
+import {
+  communityFirstComponentType,
+  communitySecondCoponentType,
+  communityStoryProps,
+} from "@/types";
+
+import { getData, postData } from ".";
+
+// 커뮤니티 스토리
+export const getCommunityFirstComponent = async (challengeId: string) => {
+  const response = await getData<communityFirstComponentType>(
+    `community/${challengeId}/participant-information`
+  );
+  return response.data;
+};
+
+// 나의 스토리
+export const getMyCommunityStory = async (challengeId: string) => {
+  const response = await getData<communityStoryProps>(
+    `community/${challengeId}/my-participant-information`
+  );
+  return response.data;
+};
+
+// 나의 스토리 한줄 소개 쓰기
+export const postMyCommunityStoryComment = async (
+  organization: string,
+  challengeId: string,
+  content: string
+) => {
+  const response = await postData(`community/${challengeId}/cheering-phrase`, {
+    organization,
+    challengeId,
+    content,
+  });
+  return response.data;
+};
+
+// 커뮤니티 활성화 날짜 배열
+export const getCommunityDate = async (challengeId: string) => {
+  const response = await getData<string[]>(`community/${challengeId}/date`);
+  return response.data;
+};
+
+// 커뮤니티 해당 날짜 데이터
+export const getCommunityContentData = async (
+  organization: string,
+  challengeId: string,
+  date: string
+) => {
+  const response = await getData<communitySecondCoponentType>(
+    `community/${challengeId}/template/${date}/${organization}`
+  );
+  return response.data;
+};
