58d3be0ecf141383a3554f2fdf75046610440e65
hoyyChoi <110888511+hoyyChoi@users.noreply.github.com>
2024-01-25 04:20:14 +0900

    fix : router

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,32 +1,24 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { useRecoilValue } from "recoil";
 
 import Layout from "@/Layout";
 import { ScrollToTop } from "@/components/Common/ScrollToTop";
-import { CompletePopupResponsive } from "@/components/atom/WritingPopup/CompletePopup";
-import ContentPopupResponsive from "@/components/atom/WritingPopup/ContentPopupResponsive";
-import { DeletePopupResponsive } from "@/components/atom/WritingPopup/DeletePopup";
 import { KakaoCallback } from "@/pages/Authorization/KakaoCallback";
 import LoginPage from "@/pages/Authorization/LoginPage";
 import RegisterEmailPage from "@/pages/Authorization/RegisterEmailPage";
 import RegisterPage from "@/pages/Authorization/RegisterPage";
 import { CommunityPage } from "@/pages/CommunityPage/CommunityPage";
 import MainPage from "@/pages/MainPage/MainPage";
+import { OnboardingPage } from "@/pages/OnboardingPage/OnboardingPage";
 import { WritingPage } from "@/pages/WritingPage/WritingPage";
-import { modalBackgroundState } from "@/recoil/atoms";
 
 const router = () => {
-  const modal = useRecoilValue(modalBackgroundState);
   const PrivateRoute = () => {
     return localStorage.getItem("accessToken") ? <MainPage /> : <Navigate to="/login" />;
   };
   return (
     <BrowserRouter>
       <ScrollToTop />
-      {modal.contentModal && <ContentPopupResponsive />}
-      {modal.deleteModal && <DeletePopupResponsive />}
-      {modal.completeModal && <CompletePopupResponsive />}
       <Routes>
         <Route
           path="/login"
@@ -43,6 +35,10 @@ const router = () => {
         <Route
           path="/auth/kakao/callback"
           element={<KakaoCallback />}
+        />
+        <Route
+          path="/onboarding"
+          element={<OnboardingPage />}
         />
         <Route element={<Layout />}>
           <Route
