b17ccae7593518bf7b5444552e766ba6fa1ab8e6
hoyyChoi <110888511+hoyyChoi@users.noreply.github.com>
2024-01-25 04:16:17 +0900

    feat : ModalProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { RecoilRoot } from "recoil";
 
+import { ModalProvider } from "./components/Common/ModalProvider";
 import Router from "./router";
 import { GlobalStyle } from "./style/global";
 const App = () => {
   return (
     <RecoilRoot>
+      <ModalProvider />
       <GlobalStyle />
       <Router />
     </RecoilRoot>
