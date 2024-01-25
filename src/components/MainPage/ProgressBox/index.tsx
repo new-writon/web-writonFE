a0ba3ef5931d805a2fb745b2d0a0f8270f1e3e08
hoyyChoi <110888511+hoyyChoi@users.noreply.github.com>
2024-01-26 03:34:05 +0900

    fix : progressBar

--- src/components/MainPage/ProgressBox/index.tsx
+++ src/components/MainPage/ProgressBox/index.tsx
@@ -18,7 +18,7 @@ export const ProgressBox = ({
   const [value, setValue] = useState<number>(0);
   const [tooltipOn, setTooltopOn] = useState<boolean>(false);
   useEffect(() => {
-    const count = ChallengeCurrent?.challengeSuccessCount || 5;
+    const count = ChallengeCurrent?.challengeSuccessCount || 0;
     const percentage = ChallengeCurrent?.challengeOverlapCount || 20;
     setTimeout(() => {
       setValue(0); // 가중치 넣기 , 1이 아무것도 안했을 때 value 나중에 날짜에 가중치 *5씩하기
@@ -38,19 +38,19 @@ export const ProgressBox = ({
         </div>
         <ProgressBar
           value={value} // 퍼센티지 받기 && 날짜만 받아서 가중치 곱해도 될듯
-          startDate={ChallengeCurrent?.challengeSuccessCount || 5} //날짜 받기
-          endDate={ChallengeCurrent?.challengeOverlapCount || 5}
+          startDate={ChallengeCurrent?.challengeSuccessCount} //날짜 받기
+          endDate={ChallengeCurrent?.challengeOverlapCount}
         />
         <CountingLabelContainer>
           <CountingLabelCard
             title={"작성된 회고"}
-            currentContent={`${ChallengeCurrent?.challengeSuccessCount || "5"}일`}
-            defaultContent={`${ChallengeCurrent?.challengeOverlapCount || "20"}일`}
+            currentContent={`${ChallengeCurrent?.challengeSuccessCount}일`}
+            defaultContent={`${ChallengeCurrent?.challengeOverlapCount}일`}
           />
           <CountingLabelCard
             title={"환급 가능 보증금"}
-            currentContent={`${ChallengeCurrent?.overlapDeposit.toLocaleString() || "25000"}원`}
-            defaultContent={`${ChallengeCurrent?.overlapDeposit.toLocaleString() || "25000"}원`}
+            currentContent={`${ChallengeCurrent?.overlapDeposit.toLocaleString()}원`}
+            defaultContent={`${ChallengeCurrent?.overlapDeposit.toLocaleString()}원`}
           />
           {/* <div className="priceMessage">
             회고 {20 - 15}일 더 작성하면, 보증급 전액 환급 가능해요.
