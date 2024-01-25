91bf8ae7db37c74460e6b1db965faab04a350cd0
hoyyChoi <110888511+hoyyChoi@users.noreply.github.com>
2024-01-25 22:33:44 +0900

    feat : CommnetAndLikeFloating

--- src/components/atom/CommentAndLikeFloating/index.tsx
+++ src/components/atom/CommentAndLikeFloating/index.tsx
@@ -1,18 +1,59 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
+import { useSetRecoilState } from "recoil";
+
+import { postLike, postLikeDelete } from "@/apis/DetailPage";
 import comment from "@/assets/DetailPage/comment.svg";
 import fireOff from "@/assets/DetailPage/fireOff.svg";
 import fireOn from "@/assets/DetailPage/fireOn.svg";
+import { LikeState } from "@/recoil/atoms";
 
 import { Container } from "./style";
 
-export const CommnetAndLikeFloating = () => {
+export const CommnetAndLikeFloating = ({
+  userTemplateId,
+  myLikeSign,
+  commentCount,
+  likeCount,
+}: {
+  userTemplateId: number;
+  myLikeSign: string;
+  commentCount: string;
+  likeCount: string;
+}) => {
   const [IsHover, setIsHover] = useState<boolean>(false);
   const [IsClick, setIsCick] = useState<boolean>(false);
+  const setLikeCount = useSetRecoilState(LikeState);
 
-  const LikeFunc = () => {
-    setIsCick(!IsClick);
+  const LikeFunc = async () => {
+    if (!IsClick) {
+      // 클릭 아직 안했을 때 좋아요수 변경 +1
+
+      const response = await postLike(
+        userTemplateId,
+        localStorage.getItem("organization") || "letsintern"
+      );
+      console.log(response);
+      setLikeCount((Number(likeCount) + 1).toString());
+      setIsCick(true);
+    } else {
+      const response = await postLikeDelete(
+        userTemplateId,
+        localStorage.getItem("organization") || "letsintern"
+      );
+      console.log(response);
+      setLikeCount((Number(likeCount) - 1).toString());
+      setIsCick(false);
+    }
   };
+
+  useEffect(() => {
+    if (myLikeSign === "1") {
+      setIsCick(true);
+    } else if (myLikeSign === "0") {
+      setIsCick(false);
+    }
+  }, [myLikeSign]);
   return (
     <Container>
       <div className="Box">
@@ -26,7 +67,7 @@ export const CommnetAndLikeFloating = () => {
             src={IsClick ? fireOn : IsHover ? fireOn : fireOff}
             alt="fire"
           />
-          <div className="num">{1}</div>
+          <div className="num">{likeCount}</div>
         </div>
         <div className="ment">응원해요</div>
       </div>
@@ -36,7 +77,7 @@ export const CommnetAndLikeFloating = () => {
             src={comment}
             alt="fire"
           />
-          <div className="num">{1}</div>
+          <div className="num">{commentCount}</div>
         </div>
         <div className="ment">댓글</div>
       </div>
