09b91690c2258f0ae71184f7c768e28f1159ba16
hoyyChoi <110888511+hoyyChoi@users.noreply.github.com>
2024-01-25 22:36:53 +0900

    fix : writeView

--- src/components/DetailPage/WriteView/index.tsx
+++ src/components/DetailPage/WriteView/index.tsx
@@ -6,15 +6,15 @@ import { communityContentProps } from "@/types";
 import { Container } from "./style";
 
 export const WriteView = ({ detailData }: { detailData: communityContentProps[] }) => {
+  const arr = detailData?.filter((item) => item.category === "스페셜 질문");
   return (
     <Container>
       <div className="date">{format(detailData[0]?.created_at, "M월 d일 회고")}</div>
-      <div className="specialQuestion">
-        <TitleSideBox type="special">스페셜 질문</TitleSideBox>
-        <div className="QuestionBox">
-          {detailData
-            .filter((item) => item.category === "스페셜 질문")
-            .map((item, idx) => (
+      {arr.length > 0 && (
+        <div className="specialQuestion">
+          <TitleSideBox type="special">스페셜 질문</TitleSideBox>
+          <div className="QuestionBox">
+            {arr.map((item, idx) => (
               <div>
                 <div className="title">
                   {idx + 1}.{item?.question}
@@ -22,8 +22,9 @@ export const WriteView = ({ detailData }: { detailData: communityContentProps[] 
                 <div className="content">{item?.content}</div>
               </div>
             ))}
+          </div>
         </div>
-      </div>
+      )}
       <div className="basicQuestion">
         <TitleSideBox type="special">베이직 질문</TitleSideBox>
         <div className="QuestionBox">
