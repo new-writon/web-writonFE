fdb284dc6d9f87b8764039ad942c98a613fcc62b
hoyyChoi <110888511+hoyyChoi@users.noreply.github.com>
2024-01-26 03:29:36 +0900

    feat : onboardingPage API

--- src/apis/OnboardingPage.ts
+++ src/apis/OnboardingPage.ts
@@ -1,7 +1,35 @@
-import { getData } from ".";
+import { getData, postData } from ".";
 
 //닉네임중복
 export const getDuplicateNickname = async (organization: string, NickName: string) => {
   const response = await getData(`/challenge/start/check/${organization}?nickname=${NickName}`);
   return response.data;
 };
+
+interface onBoardingDataProps {
+  nickname: string;
+  job: string;
+  jobIntroduce: string;
+  hireDate: string;
+  company: string;
+  companyPublic: string;
+  organization: string;
+}
+export const postOnboardingComplete = async (onBoardingData: onBoardingDataProps) => {
+  const response = await postData("/challenge/start/enroll", onBoardingData);
+  return response.data;
+};
+
+export const postChallengeStart = async (organization: string, challengeId: string) => {
+  const response = await postData("/challenge/start", { organization, challengeId });
+  return response.data;
+};
+
+// {
+//   nickname,
+//   job,
+//   jobIntroduce,
+//   hireDate,
+//   company,
+//   companyPublic,
+// }
