b56055a4a6edfafc8d6ced426e845a8812470a5d
hoyyChoi <110888511+hoyyChoi@users.noreply.github.com>
2024-01-29 22:04:48 +0900

    fix : myRetrospectItem click Mobile

--- src/components/atom/RetrospectItem/index.tsx
+++ src/components/atom/RetrospectItem/index.tsx
@@ -1,5 +1,8 @@
+import { useEffect, useState } from "react";
+
 import { format } from "date-fns";
 import { ko } from "date-fns/locale";
+import { useNavigate } from "react-router-dom";
 import { useSetRecoilState } from "recoil";
 
 import { DetailDataState, DetailModalState, LikeState } from "@/recoil/atoms";
@@ -14,12 +17,29 @@ export const RetrospectItem = ({ data }: { data: communityContentProps[] }) => {
   const setDetailData = useSetRecoilState(DetailDataState);
   const setDetailModal = useSetRecoilState(DetailModalState);
   const setLikeCount = useSetRecoilState(LikeState);
+  const [width, setWidth] = useState<number>(window.innerWidth);
+  const navigate = useNavigate();
+
   const spaceToDetail = () => {
-    setDetailData(data);
-    setLikeCount(data[0]?.likeCount);
-    setDetailModal(true);
+    if (width <= 530) {
+      navigate(`/detail/${data[0].user_templete_id}`);
+      // setDetailData(data);
+      // setLikeCount(data[0]?.likeCount);
+    } else {
+      setDetailData(data);
+      setLikeCount(data[0]?.likeCount);
+      setDetailModal(true);
+    }
   };
 
+  const handleResize = () => {
+    //뷰크기 강제로 강져오기
+    setWidth(window.innerWidth);
+  };
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize); //clean
+  }, [width]);
   return (
     <Container onClick={spaceToDetail}>
       <Title>
