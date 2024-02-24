f4fbe583c10acac07b9e30c7e9bc30654ab5576e
hoyyChoi <110888511+hoyyChoi@users.noreply.github.com>
2024-02-25 03:03:04 +0900

    feat : profileSetting component

---
+++ src/components/MyPage/ProfileSetting/index.tsx
@@ -0,0 +1,357 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { ChangeEvent, useEffect, useRef, useState } from "react";
+
+import { format } from "date-fns";
+import { useSetRecoilState } from "recoil";
+
+import { getDuplicateNickname } from "@/apis/OnboardingPage";
+import { KeywordButton, PublicButton } from "@/components/atom/button";
+import { MyPageInput } from "@/components/atom/input/style";
+import { accountNumberState } from "@/recoil/atoms";
+import { communityStoryProps } from "@/types";
+
+import {
+  AccountButton,
+  BasicSetting,
+  ChallengeSetting,
+  Container,
+  DuplicateBtn,
+  EditBox,
+  EditButton,
+  Top,
+} from "./style";
+
+const JobCategory = ["기획", "운영", "경영", "개발", "마케팅", "디자인"];
+
+interface ProfileSettingProps {
+  nickname: string | undefined;
+  job: string | undefined;
+  jobIntroduce: string | undefined;
+  hireDate: string | undefined;
+  company: string | undefined | null;
+  companyPublic: number | undefined | null;
+}
+
+export const ProfileSetting = ({ myData }: { myData: communityStoryProps | undefined }) => {
+  const [editActive, setEditActive] = useState<boolean>(false);
+  const [ProfileData, setProfileData] = useState<ProfileSettingProps>({
+    nickname: "",
+    job: "",
+    jobIntroduce: "",
+    hireDate: "",
+    company: "",
+    companyPublic: 0,
+  });
+  const [duplicateShow, setDuplicateShow] = useState<boolean>(false);
+  const [duplicate, setDuplicate] = useState<boolean>(false);
+  const [errorIdLine, setErrorIdLine] = useState<boolean>(false);
+  const textareaRef = useRef<HTMLTextAreaElement | null>(null);
+  const setAccountNumberModal = useSetRecoilState(accountNumberState);
+
+  const handleNicknameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setDuplicateShow(false);
+    setErrorIdLine(false);
+    // 정규식을 사용하여 한글, 영어, 숫자만 추출
+    const validCharacters = value.match(/[ㄱ-ㅎ|ㅏ-ㅣ|가-힣a-zA-Z0-9]/g) || [];
+
+    if (value.length < 16) {
+      setProfileData({ ...ProfileData, nickname: validCharacters.join("") }); // 추출된 문자를 다시 합침
+    }
+  };
+  const DuplicateCheck = async () => {
+    try {
+      const response = await getDuplicateNickname(
+        localStorage.getItem("organization") || "letsintern",
+        ProfileData.nickname
+      );
+      console.log(response);
+      setDuplicate(true);
+      setDuplicateShow(true); // 일단 버튼을 누르면 hide 클래스 제거
+    } catch (err) {
+      setDuplicate(false);
+      setDuplicateShow(true); // 일단 버튼을 누르면 hide 클래스 제거
+      setErrorIdLine(true); //에러 라인 전달
+      throw new Error("shit");
+    }
+  };
+
+  const handleCompanyChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    // 정규식을 사용하여 한글, 영어, 숫자만 추출
+    const validCharacters = value.match(/[ㄱ-ㅎ|ㅏ-ㅣ|가-힣a-zA-Z0-9]/g) || [];
+
+    if (value.length < 21) {
+      setProfileData({ ...ProfileData, company: validCharacters.join("") }); // 추출된 문자를 다시 합침
+    }
+  };
+
+  const handleHireDateChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const digitsOnly = e.target.value.replace(/\D/g, "");
+
+    // 정해진 날짜 형식에 따라 "-" 추가
+    let formattedDate = digitsOnly.replace(/(\d{4})(\d{2})(\d{2})/, "$1-$2-$3");
+
+    // 추가로 하이픈을 포함시키는 로직 (2024 이후에 하이픈 추가)
+    if (digitsOnly.length >= 8) {
+      formattedDate = formattedDate.replace(/(\d{4}-\d{2})/, "$1");
+    }
+
+    // 일 입력 시 이후 입력을 막는 로직
+    if (digitsOnly.length >= 10) {
+      formattedDate = formattedDate.substring(0, 10);
+    }
+    if (formattedDate.length < 11) {
+      setProfileData({ ...ProfileData, hireDate: formattedDate });
+    }
+  };
+  const handleJobIntroduceChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    const value = e.target.value;
+
+    const validCharacters =
+      value.match(/[ㄱ-ㅎ|ㅏ-ㅣ|가-힣a-zA-Z0-9\s!"#$%&'()*+,-./:;<=>?@[\\\]^_`{|}~]/g) || [];
+
+    if (value.length < 51) {
+      setProfileData({ ...ProfileData, jobIntroduce: validCharacters.join("") }); // 추출된 문자를 다시 합침
+    }
+  };
+
+  const EditComplete = () => {
+    // 수정 api 발송 ProfileData
+    setEditActive(false);
+    console.log(ProfileData);
+    window.location.reload();
+    window.scrollTo({ top: 0 });
+  };
+
+  useEffect(() => {
+    setProfileData({
+      ...ProfileData,
+      nickname: myData?.nickname,
+      job: myData?.job,
+      jobIntroduce: myData?.job_introduce,
+      hireDate: format(myData?.cheering_phrase_date || new Date(), "yyyy-MM-dd"),
+      company: myData?.company,
+      companyPublic: myData?.company_public,
+    });
+  }, [myData, editActive]);
+
+  return (
+    <Container>
+      <Top>
+        <div className="title">기본 정보</div>
+        <div
+          className={`${editActive ? "active" : "notactive"} editBtn`}
+          onClick={() => setEditActive(true)}
+        >
+          {editActive ? "수정중" : "내 정보 수정"}
+        </div>
+      </Top>
+      {!editActive ? (
+        <>
+          <BasicSetting>
+            <div className="editField">
+              <div className="editTitle">닉네임</div>
+              <div className="editText">{myData?.nickname}</div>
+            </div>
+            <div className="editField">
+              <div className="editTitle">이메일</div>
+              <div className="editText">{myData?.email}</div>
+            </div>
+            <div className="editField">
+              <div className="editTitle">계좌번호</div>
+              <div className="editText account">
+                계좌번호를 입력해주세요.
+                <AccountButton
+                  onClick={() => {
+                    setAccountNumberModal(true);
+                    document.body.style.overflowY = "hidden";
+                  }}
+                >
+                  추가
+                </AccountButton>
+                {/* 계좌번호 데이터가 없으면 입력해주세요 글씨 , 추가 버튼 만약 있으면 계좌번호랑 변경 버튼 */}
+              </div>
+            </div>
+          </BasicSetting>
+          <ChallengeSetting>
+            <div className="title">챌린지 참여 정보</div>
+
+            <div className="editField">
+              <div className="editTitle">회사명</div>
+              <div className="editText">
+                {myData?.company}
+                <PublicButton
+                  onClick={() => {}}
+                  secret={myData?.company_public}
+                  state="default"
+                />
+              </div>
+            </div>
+            <div className="editField">
+              <div className="editTitle">입사 날짜</div>
+              <div className="editText">
+                {format(myData?.cheering_phrase_date || new Date(), "yyyy-MM-dd")}
+              </div>
+            </div>
+            <div className="editField">
+              <div className="editTitle">직무</div>
+              <div className="editJob">
+                <KeywordButton
+                  onClick={() => {}}
+                  select={false}
+                >
+                  {myData?.job}
+                </KeywordButton>
+              </div>
+            </div>
+            <div className="editField">
+              <div className="editTitle">직무에 대한 한 줄 소개</div>
+              <div className="editText">{myData?.job_introduce}</div>
+            </div>
+          </ChallengeSetting>
+        </>
+      ) : (
+        <>
+          <BasicSetting>
+            <div className="editField">
+              <div className="editTitle">닉네임</div>
+              <EditBox>
+                <div className="inputNickname">
+                  <MyPageInput
+                    type="text"
+                    value={ProfileData.nickname || ""}
+                    onChange={handleNicknameChange}
+                    placeholder="닉네임을 입력해주세요."
+                    errorLine={errorIdLine}
+                  />
+                  <DuplicateBtn
+                    className={duplicateShow ? "dupliBtn" : ""}
+                    onClick={DuplicateCheck}
+                  >
+                    중복확인
+                  </DuplicateBtn>
+                </div>
+                <div className="parityCheck">
+                  <div className="textCheck">
+                    {duplicateShow ? (
+                      duplicate ? (
+                        <p className="success">사용 가능한 닉네임이에요.</p>
+                      ) : (
+                        <p className="error">사용할 수 없는 닉네임이에요.</p>
+                      )
+                    ) : (
+                      ""
+                    )}
+                  </div>
+                  <div className="numCheck">({ProfileData?.nickname?.length}/15)</div>
+                </div>
+              </EditBox>
+            </div>
+            <div className="editField">
+              <div className="editTitle">이메일</div>
+              <div className="editText editEmail">{myData?.email}</div>
+            </div>
+            <div className="editField">
+              <div className="editTitle">계좌번호</div>
+              <div className="editText account">
+                계좌번호를 입력해주세요.
+                <AccountButton
+                  onClick={() => {
+                    setAccountNumberModal(true);
+                    document.body.style.overflowY = "hidden";
+                  }}
+                >
+                  추가
+                </AccountButton>
+              </div>
+            </div>
+          </BasicSetting>
+          <ChallengeSetting>
+            <div className="title">챌린지 참여 정보</div>
+
+            <div className="editField">
+              <div className="editTitle">회사명</div>
+              <EditBox>
+                <MyPageInput
+                  type="text"
+                  value={ProfileData.company || ""}
+                  onChange={handleCompanyChange}
+                  placeholder="근무중인 회사 이름을 입력해주세요."
+                />
+                <PublicButton
+                  onClick={() =>
+                    setProfileData({
+                      ...ProfileData,
+                      companyPublic: ProfileData.companyPublic === 1 ? 0 : 1,
+                    })
+                  }
+                  secret={ProfileData?.companyPublic}
+                  state="edit"
+                />
+              </EditBox>
+            </div>
+            <div className="editField">
+              <div className="editTitle">입사 날짜</div>
+              <EditBox>
+                <MyPageInput
+                  type="text"
+                  value={ProfileData.hireDate || ""}
+                  onChange={handleHireDateChange}
+                  placeholder="20240101"
+                />
+              </EditBox>
+            </div>
+            <div className="editField jobField">
+              <div className="editTitle">직무</div>
+              <div className="editJob">
+                {JobCategory.map((item, idx) => (
+                  <React.Fragment key={idx}>
+                    <KeywordButton
+                      onClick={() => setProfileData({ ...ProfileData, job: item })}
+                      select={ProfileData.job === item}
+                    >
+                      {item}
+                    </KeywordButton>
+                  </React.Fragment>
+                ))}
+              </div>
+            </div>
+            <div className="editField jobIntroduceField">
+              <div className="editTitle">직무에 대한 한 줄 소개</div>
+              <EditBox>
+                <textarea
+                  ref={textareaRef}
+                  value={ProfileData.jobIntroduce}
+                  onChange={(e) => handleJobIntroduceChange(e)}
+                  placeholder="어떤 일을 하나요? 간단하게 소개해주세요. ex) 부트캠프 division의 UX/UI 디자인 시스템 구축 업무 어시스트"
+                />
+                <div className="parityCheck">
+                  <div></div>
+                  <div className="numCheck">({ProfileData.jobIntroduce?.length}/50)</div>
+                </div>
+              </EditBox>
+            </div>
+          </ChallengeSetting>
+          <EditButton>
+            <div
+              className="editCloseBtn"
+              onClick={() => {
+                setEditActive(false);
+                window.scrollTo({ top: 0 });
+              }}
+            >
+              취소
+            </div>
+            <div
+              className="editCompleteBtn"
+              onClick={EditComplete}
+            >
+              수정 완료
+            </div>
+          </EditButton>
+        </>
+      )}
+    </Container>
+  );
+};
---
+++ src/components/MyPage/ProfileSetting/style.ts
@@ -0,0 +1,222 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  background-color: #fff;
+  width: 805px;
+  border-radius: 16px;
+  margin-bottom: 300px;
+  padding: 50px 50px 80px 60px;
+  .title {
+    color: #000;
+    font-size: 1.125rem;
+    font-weight: 600;
+    line-height: 130%; /* 23.4px */
+  }
+  .editField {
+    display: flex;
+    padding: 15px 0;
+    max-width: 443px;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .editTitle {
+    color: var(--Gray-60, #959595);
+    font-size: 0.875rem;
+    font-weight: 600;
+    line-height: 160%; /* 22.4px */
+  }
+  .editText {
+    width: 300px;
+    height: 40px;
+    padding: 15px;
+    gap: 10px;
+    border-radius: 8px;
+    background: var(--Gray-10, #fafafa);
+    color: var(--Gray-100, #212121);
+    font-size: 0.875rem;
+    font-weight: 600;
+  }
+  .editText.account {
+    color: var(--Gray-60, #959595);
+    position: relative;
+  }
+  .editJob {
+    width: 300px;
+    display: flex;
+  }
+  .editEmail {
+    color: var(--Gray-50, #bdbdbd);
+  }
+`;
+
+export const Top = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  .editBtn {
+    padding: 9px 12px 7px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 10px;
+    text-align: center;
+    font-size: 0.875rem;
+    font-weight: 600;
+    line-height: 160%; /* 22.4px */
+    cursor: pointer;
+  }
+  .editBtn.notactive {
+    background: var(--purple-50, #6a63f5);
+    color: var(--White, #fff);
+  }
+  .editBtn.active {
+    background: var(--Gray-30, #eee);
+    color: var(--Gray-60, #959595);
+  }
+`;
+
+export const BasicSetting = styled.div``;
+
+export const ChallengeSetting = styled.div`
+  margin-top: 40px;
+  .jobField {
+    max-width: 480px;
+    .editJob {
+      width: max-content;
+      display: flex;
+      gap: 8px;
+    }
+  }
+  .jobIntroduceField {
+    align-items: flex-start;
+  }
+  textarea {
+    width: 100%;
+    height: 104px;
+    border-radius: 8px;
+    border: 1px solid var(--Gray-40, #d9d9d9);
+    background: var(--White, #ffff);
+    outline: none;
+    padding: 16px;
+    box-sizing: border-box;
+    resize: none;
+    font-size: 1rem;
+    caret-color: #6a63f5;
+    color: var(--Gray-100, #212121);
+    line-height: 24px;
+    overflow-y: hidden;
+    &::placeholder {
+      color: var(--Gray-60, #959595);
+      font-size: 1rem;
+      line-height: 150%; /* 24px */
+      white-space: pre-wrap;
+    }
+    &:focus {
+      &::placeholder {
+        opacity: 0;
+      }
+    }
+  }
+`;
+
+export const EditBox = styled.div`
+  width: 300px;
+  position: relative;
+  .inputNickname {
+  }
+  .error {
+    color: var(--Error_50, #dc362e);
+  }
+  .success {
+    color: var(--purple-50, #6a63f5);
+  }
+  .dupliBtn {
+    color: var(--Gray-60, #959595);
+  }
+  .title {
+    color: var(--Gray10_900, #212121);
+    font-size: 0.875rem;
+    font-weight: 600;
+    line-height: 160%;
+  }
+  .parityCheck {
+    width: 100%;
+    position: absolute;
+    margin-top: 6px;
+    display: flex;
+    font-size: 0.75rem;
+    p {
+      font-size: 0.75rem;
+    }
+  }
+  .textCheck {
+    position: absolute;
+    left: 0;
+  }
+  .numCheck {
+    position: absolute;
+    right: 0;
+    font-size: 0.75rem;
+    color: var(--Gray-60, #959595);
+  }
+  input {
+    border-radius: 8px;
+    border: 1px solid var(--Gray-40, #d9d9d9);
+    background: var(--White, #ffff);
+  }
+`;
+
+export const DuplicateBtn = styled.button`
+  border: 0;
+  background-color: transparent;
+  font-size: 0.875rem;
+  padding: 0;
+  position: absolute;
+  top: 50%;
+  transform: translate(0, -50%);
+  right: 16px;
+  color: var(--Shadow_Blue, #135ff3);
+  cursor: pointer;
+`;
+
+export const EditButton = styled.div`
+  margin-top: 77px;
+  display: flex;
+  gap: 16px;
+  justify-content: center;
+  div {
+    padding: 16px 30px;
+    border-radius: 12px;
+    font-size: 1rem;
+    font-weight: 600;
+    line-height: 150%; /* 24px */
+    cursor: pointer;
+  }
+  .editCloseBtn {
+    background: var(--Purple-10, #f0efff);
+    color: var(--purple-50, #6a63f5);
+  }
+  .editCompleteBtn {
+    background: var(--purple-50, #6a63f5);
+    color: var(--White, #fff);
+  }
+`;
+
+export const AccountButton = styled.div`
+  padding: 9px 12px 7px;
+  border-radius: 10px;
+
+  font-size: 0.875rem;
+  font-weight: 600;
+  line-height: 160%; /* 22.4px */
+  position: absolute;
+  right: -80px;
+  top: 0;
+  cursor: pointer;
+  background: var(--Purple-10, #f0efff);
+  color: var(--purple-50, #6a63f5);
+  &:hover {
+    background: var(--purple-50, #6a63f5);
+    color: var(--White, #fff);
+  }
+`;
