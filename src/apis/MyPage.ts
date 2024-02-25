da07784dfb8dadde379a5bf84347acda30f3c0b6
hoyyChoi <110888511+hoyyChoi@users.noreply.github.com>
2024-02-26 01:33:07 +0900

    feat : myPage API

---
+++ src/apis/MyPage.ts
@@ -0,0 +1,21 @@
+import { accountNumberProps, myPageProps, myProfileEditProps } from "@/types";
+
+import { getData, patchData, putData } from ".";
+
+//마이페이지 내 정보 조회
+export const getMyPageData = async (organization: string) => {
+  const response = await getData<myPageProps>(`/user/${organization}/my-profile`);
+  return response.data;
+};
+
+// 마이페이지 프로필 정보 수정
+export const putMyPageData = async (organization: string, editData: myProfileEditProps) => {
+  const response = await putData(`/user/${organization}/my-profile`, editData);
+  return response.data;
+};
+
+//계좌번호
+export const patchAccountNumberData = async (accountNumberData: accountNumberProps) => {
+  const response = await patchData("/user/account-number", accountNumberData);
+  return response.data;
+};
