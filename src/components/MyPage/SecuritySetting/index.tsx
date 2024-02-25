5147a596ec6d8c8916e0922f6f0e5c8a67def036
hoyyChoi <110888511+hoyyChoi@users.noreply.github.com>
2024-02-26 01:34:23 +0900

    feat : securitySetting component

--- src/components/MyPage/SecuritySetting/index.tsx
+++ src/components/MyPage/SecuritySetting/index.tsx
@@ -1,5 +1,18 @@
 import { Container } from "./style";
 
 export const SecuritySetting = () => {
-  return <Container></Container>;
+  return (
+    <Container>
+      <div className="title">보안 설정</div>
+      <div className="editField">
+        <div className="editTitle">비밀번호</div>
+        <div
+          className="editPasswordBtn"
+          onClick={() => alert("아직 준비중입니다!")}
+        >
+          비밀번호 변경
+        </div>
+      </div>
+    </Container>
+  );
 };
