a658c6060726191c6547e7f59dd92279e93eb999
hoyyChoi <110888511+hoyyChoi@users.noreply.github.com>
2024-02-28 16:41:17 +0900

    feat : communityBox -> Category, calendar

--- src/components/CommunityPage/CommunityBox/index.tsx
+++ src/components/CommunityPage/CommunityBox/index.tsx
@@ -2,14 +2,23 @@
 import React, { useEffect, useState } from "react";
 
 import { format } from "date-fns";
+import moment from "moment";
+import Calendar from "react-calendar";
 import { useRecoilState } from "recoil";
+import "react-calendar/dist/Calendar.css";
 
 import { getCommunityContentData, getCommunityDate } from "@/apis/CommunityPage";
+import downArrowActive from "@/assets/communityPage/downArrowActive.svg";
+import calendarIMG from "@/assets/communityPage/icon-calendar.svg";
 import arrow from "@/assets/communityPage/storyArrow.svg";
+import topArrowActive from "@/assets/communityPage/topArrowActive.svg";
+import downArrow from "@/assets/mainPage/downArrow.svg";
+import topArrow from "@/assets/mainPage/topArrow.svg";
 import { NoRetrospect } from "@/components/MainPage/NoRetrospect";
 import { CommunityItem } from "@/components/atom/CommunityItem";
 import { MainSemiTitle } from "@/components/atom/MainSemiTitle";
 import { TitleSideBox } from "@/components/atom/TitleSideBox";
+import { KeywordButton } from "@/components/atom/button";
 import useAsyncWithLoading from "@/hooks/useAsyncWithLoading";
 import {
   CommunitySecondDataState,
@@ -18,9 +27,11 @@ import {
   dateLengthState,
 } from "@/recoil/atoms";
 import { Inner } from "@/style/global";
-import { communitySecondCoponentType } from "@/types";
+import { communityContentProps, communitySecondCoponentType } from "@/types";
 
 import { CommunityHeader, CommunityItemBox, Container } from "./style";
+
+const JobCategory = ["기획", "운영", "개발", "마케팅", "홍보", "디자인"];
 
 export const CommunityBox = () => {
   // const today = new Date();
@@ -31,7 +42,80 @@ export const CommunityBox = () => {
   const [dateLastLength, setDateLastLength] = useState<number>(0);
   const [CommunitySecondData, setCommunitySecondData] =
     useRecoilState<communitySecondCoponentType>(CommunitySecondDataState);
+  const [filteredData, setFilteredData] = useState<communityContentProps[][]>([]);
+  const [calendarOn, setCalendarOn] = useState<boolean>(false);
+  const [categoriesOn, setCategoriesOn] = useState<boolean>(false);
+  const [mobileCategoriesActive, setMobileCategoriesActive] = useState<boolean>(false);
+
   const executeAsyncTask = useAsyncWithLoading();
+  const [today, setToday] = useState(new Date());
+  const [categoriesArray, setCategoriesArray] = useState(["전체"]);
+
+  const ChangeCategories = (item: string) => {
+    if (item !== "전체") {
+      setMobileCategoriesActive(true);
+      setCategoriesArray((prevCategories) => {
+        // '전체'를 제외한 모든 값들을 유지하고 item이 이미 있는지 확인하여 처리
+        if (prevCategories.includes(item)) {
+          // item이 이미 존재하면 item을 배열에서 제거하고 '전체'를 추가
+          const updatedCategories = prevCategories.filter((element: string) => element !== item);
+          // 모든 카테고리가 해제되었을 때 배열에 "전체" 추가
+          if (updatedCategories.length === 0) {
+            setMobileCategoriesActive(false);
+            return ["전체"];
+          } else {
+            return updatedCategories;
+          }
+        } else {
+          // item이 존재하지 않으면 '전체'를 제거하고 item을 배열에 추가
+          return [...prevCategories.filter((element: string) => element !== "전체"), item];
+        }
+      });
+    } else {
+      setMobileCategoriesActive(false);
+      setCategoriesArray(["전체"]);
+    }
+  };
+
+  useEffect(() => {
+    if (categoriesArray.includes("전체")) {
+      setFilteredData(CommunitySecondData.templateData); // "전체"를 선택한 경우, 모든 데이터를 보여줍니다.
+    } else {
+      const filtered = CommunitySecondData.templateData.filter((item) =>
+        categoriesArray.includes(item[0].job)
+      ); // 선택한 카테고리에 해당하는 데이터를 필터링합니다.
+      setFilteredData(filtered);
+    }
+  }, [categoriesArray]);
+
+  const clickDay = (value: Date) => {
+    executeAsyncTask(async () => {
+      try {
+        const result = await getCommunityContentData(
+          localStorage.getItem("organization") || "",
+          localStorage.getItem("challengeId") || "1",
+          format(value, "yyyy-MM-dd")
+        );
+        localStorage.setItem("date", format(value, "yyyy-MM-dd"));
+
+        setToday(value);
+        setCommunitySecondData(result);
+
+        // 카테고리에 따른 글 필터링
+        if (categoriesArray.includes("전체")) {
+          setFilteredData(result.templateData); // "전체"를 선택한 경우, 모든 데이터를 보여줍니다.
+        } else {
+          const filtered = result.templateData.filter((item) =>
+            categoriesArray.includes(item[0].job)
+          ); // 선택한 카테고리에 해당하는 데이터를 필터링합니다.
+          setFilteredData(filtered);
+        }
+        setCalendarOn(false);
+      } catch {
+        throw new Error("shit");
+      }
+    });
+  };
 
   const CommunitySecondRendering = async () => {
     executeAsyncTask(async () => {
@@ -49,6 +133,16 @@ export const CommunityBox = () => {
             dateArray[dateArray.length - 1]
           );
           setCommunitySecondData(result);
+
+          // 카테고리에 따른 글 필터링
+          if (categoriesArray.includes("전체")) {
+            setFilteredData(result.templateData); // "전체"를 선택한 경우, 모든 데이터를 보여줍니다.
+          } else {
+            const filtered = result.templateData.filter((item) =>
+              categoriesArray.includes(item[0].job)
+            ); // 선택한 카테고리에 해당하는 데이터를 필터링합니다.
+            setFilteredData(filtered);
+          }
         } catch {
           throw new Error("shit");
         }
@@ -67,6 +161,16 @@ export const CommunityBox = () => {
           dateActive[dateLength]
         );
         setCommunitySecondData(result);
+
+        // 카테고리에 따른 글 필터링
+        if (categoriesArray.includes("전체")) {
+          setFilteredData(result.templateData); // "전체"를 선택한 경우, 모든 데이터를 보여줍니다.
+        } else {
+          const filtered = result.templateData.filter((item) =>
+            categoriesArray.includes(item[0].job)
+          ); // 선택한 카테고리에 해당하는 데이터를 필터링합니다.
+          setFilteredData(filtered);
+        }
       } catch {
         throw new Error("shit");
       }
@@ -103,6 +207,9 @@ export const CommunityBox = () => {
   };
 
   useEffect(() => {
+    if (width >= 690) {
+      setCategoriesOn(false);
+    }
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize); //clean
   }, [width]);
@@ -118,6 +225,7 @@ export const CommunityBox = () => {
   useEffect(() => {
     ChangeDate();
     localStorage.setItem("date", dateActive[dateLength]);
+    localStorage.setItem("categories", JSON.stringify(["전체"]));
   }, [dateLength]);
 
   if (!dateActive || dateLength === -1) {
@@ -137,10 +245,11 @@ export const CommunityBox = () => {
       <Container>
         <CommunityHeader>
           <div className="title">
-            {width > 560 ? (
+            {width > 625 ? (
               <>
                 <MainSemiTitle font={1.25}>
-                  {format(dateActive[dateLength], "M")}월 {format(dateActive[dateLength], "d")}일,
+                  {format(localStorage.getItem("date") || dateActive[dateLength], "M")}월{" "}
+                  {format(localStorage.getItem("date") || dateActive[dateLength], "d")}일,
                   <div className="number">{CommunitySecondData?.challengeCompleteCount}</div>
                   명이 챌린지를 완료했어요.
                 </MainSemiTitle>
@@ -149,7 +258,8 @@ export const CommunityBox = () => {
             ) : (
               <>
                 <MainSemiTitle font={1.25}>
-                  {format(dateActive[dateLength], "M")}월 {format(dateActive[dateLength], "d")}일,
+                  {format(localStorage.getItem("date") || dateActive[dateLength], "M")}월{" "}
+                  {format(localStorage.getItem("date") || dateActive[dateLength], "d")}일,
                   <div className="flex">
                     <div className="number">{CommunitySecondData?.challengeCompleteCount}</div>
                     명이 챌린지를 완료했어요.
@@ -158,30 +268,120 @@ export const CommunityBox = () => {
               </>
             )}
           </div>
-          <div className="changeDate">
-            <img
-              className="previous"
-              src={arrow}
-              alt="<"
-              onClick={() => CommunitySpaceDate("previous")}
-            />
-            <div
-              className="today"
-              onClick={() => CommunitySpaceDate("today")}
-            >
-              오늘
+          <div className="categoryAndDate">
+            {width <= 690 ? (
+              <div
+                className={`${mobileCategoriesActive && "categoriesMobileActvie"} categoriesMobile`}
+                onClick={() => setCategoriesOn(!categoriesOn)}
+              >
+                <p>직무별</p>
+                <img
+                  src={
+                    mobileCategoriesActive
+                      ? categoriesOn
+                        ? topArrowActive
+                        : downArrowActive
+                      : categoriesOn
+                        ? topArrow
+                        : downArrow
+                  }
+                  alt="v"
+                />
+              </div>
+            ) : (
+              <div className="categories">
+                <KeywordButton
+                  onClick={() => ChangeCategories("전체")}
+                  select={categoriesArray.includes("전체")}
+                >
+                  전체
+                </KeywordButton>
+                <div className="line"></div>
+                {JobCategory.map((item, idx) => (
+                  <React.Fragment key={idx}>
+                    <KeywordButton
+                      onClick={() => ChangeCategories(item)}
+                      select={categoriesArray.includes(item)}
+                    >
+                      {item}
+                    </KeywordButton>
+                  </React.Fragment>
+                ))}
+              </div>
+            )}
+            <div className="changeDate">
+              {calendarOn && (
+                <Calendar
+                  locale="ko"
+                  formatDay={(_locale, date) => moment(date).format("D")}
+                  onChange={() => setToday(today)}
+                  value={today}
+                  minDate={new Date(dateActive[0])}
+                  maxDate={new Date(dateActive[dateLastLength])}
+                  minDetail="month" // 상단 네비게이션에서 '월' 단위만 보이게 설정
+                  maxDetail="month" // 상단 네비게이션에서 '월' 단위만 보이게 설정
+                  onClickDay={clickDay}
+                  goToRangeStartOnSelect={true}
+                />
+              )}
+              <div
+                className="calendar"
+                onClick={() => setCalendarOn(!calendarOn)}
+              >
+                <img
+                  src={calendarIMG}
+                  alt="달력"
+                />
+                <img
+                  src={calendarOn ? topArrow : downArrow}
+                  alt="v"
+                />
+              </div>
+              <div
+                className="today"
+                onClick={() => CommunitySpaceDate("today")}
+              >
+                오늘
+              </div>
+              <img
+                className="previous"
+                src={arrow}
+                alt="<"
+                onClick={() => CommunitySpaceDate("previous")}
+              />
+
+              <img
+                className="next"
+                src={arrow}
+                alt="<"
+                onClick={() => CommunitySpaceDate("next")}
+              />
             </div>
-            <img
-              className="next"
-              src={arrow}
-              alt="<"
-              onClick={() => CommunitySpaceDate("next")}
-            />
           </div>
+          {categoriesOn && (
+            <div className="categoriesMobileView">
+              <KeywordButton
+                onClick={() => ChangeCategories("전체")}
+                select={categoriesArray.includes("전체")}
+              >
+                전체
+              </KeywordButton>
+              {JobCategory.map((item, idx) => (
+                <React.Fragment key={idx}>
+                  <KeywordButton
+                    onClick={() => ChangeCategories(item)}
+                    select={categoriesArray.includes(item)}
+                  >
+                    {item}
+                  </KeywordButton>
+                </React.Fragment>
+              ))}
+            </div>
+          )}
         </CommunityHeader>
-        {CommunitySecondData?.templateData?.length !== 0 ? (
+        {filteredData?.length !== 0 ? (
           <CommunityItemBox>
-            {CommunitySecondData?.templateData.map((item, idx) => (
+            {filteredData?.map((item, idx) => (
               <React.Fragment key={idx}>
                 <CommunityItem data={item} />
               </React.Fragment>
--- src/components/CommunityPage/CommunityBox/style.ts
+++ src/components/CommunityPage/CommunityBox/style.ts
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  position: relative;
   .nodata {
     margin-top: 80px;
   }
@@ -11,7 +12,7 @@ export const Container = styled.div`
 
 export const CommunityHeader = styled.div`
   display: flex;
-  justify-content: space-between;
+  flex-direction: column;
   padding: 31px 0 21px;
   position: -webkit-sticky;
   position: sticky;
@@ -19,7 +20,9 @@ export const CommunityHeader = styled.div`
   z-index: 9999;
   border-bottom: 1px solid var(--Gray-30);
   background-color: var(--Gray-20, #f5f5f5);
-  padding-left: 26px;
+  @media (min-width: 691px) {
+    padding-left: 26px;
+  }
   @media (max-width: 530px) {
     top: 105px;
     padding-left: 0;
@@ -27,6 +30,7 @@ export const CommunityHeader = styled.div`
   .title {
     display: flex;
     gap: 6px;
+    align-items: center;
   }
   .flex {
     display: flex;
@@ -35,18 +39,96 @@ export const CommunityHeader = styled.div`
   .flex .number {
     margin: 0;
   }
+  .categoryAndDate {
+    margin-top: 16px;
+    display: flex;
+    justify-content: space-between;
+  }
+  .categoriesMobile {
+    display: flex;
+    gap: 4px;
+    p {
+      color: var(--Gray-80, #616161);
+      font-size: 14px;
+      font-style: normal;
+      font-weight: 500;
+      line-height: 10px;
+    }
+    cursor: pointer;
+    padding: 10px;
+    border-radius: 6px;
+    border: 1px solid var(--Gray-40, #d9d9d9);
+    height: 30px;
+  }
+  .categoriesMobileActvie {
+    border: 1px solid var(--purple-50, #6a63f5);
+    background: var(--Purple-0, #f8f8ff);
+    p {
+      color: var(--purple-50, #6a63f5);
+    }
+  }
+
+  .categoriesMobileView {
+    margin-top: 12px;
+    display: flex;
+    gap: 8px;
+    align-items: center;
+    overflow-x: scroll;
+    overflow-y: hidden;
+    white-space: nowrap;
+
+    &::-webkit-scrollbar {
+      display: none;
+    }
+  }
+
+  .categories {
+    display: flex;
+    gap: 8px;
+    align-items: center;
+  }
+  .line {
+    margin: 0 4px;
+    width: 1px;
+    height: 24px;
+    background: var(--Gray-40, #d9d9d9);
+  }
+
   .changeDate {
     display: flex;
     align-items: center;
+    gap: 10px;
+    position: relative;
+  }
+  .changeDate .calendar {
+    border-radius: 6px;
+    border: 1px solid var(--Gray-40, #d9d9d9);
+    padding: 3px 8px 3px 6px;
+    display: flex;
+    align-items: center;
+    gap: 4px;
+    cursor: pointer;
+  }
+  .changeDate .calendar:hover {
+    background: var(--Gray-30, #eee);
+  }
+  .changeDate .calendar img[alt="v"] {
+    margin-top: 2px;
   }
   .changeDate .today {
     color: var(--Gray-80, #616161);
-    font-size: 14px;
+    font-size: 0.875rem;
     font-style: normal;
     font-weight: 500;
-    line-height: 22px;
+    line-height: 10px;
     cursor: pointer;
-    margin: 0 8px;
+    padding: 10px;
+    border-radius: 6px;
+    border: 1px solid var(--Gray-40, #d9d9d9);
+    height: 30px;
+  }
+  .changeDate .today:hover {
+    background: var(--Gray-10, #fafafa);
   }
   .changeDate .previous {
     cursor: pointer;
@@ -55,6 +137,79 @@ export const CommunityHeader = styled.div`
   .changeDate .next {
     cursor: pointer;
     transform: rotate(180deg);
+  }
+
+  //캘린더 css 적용
+  .react-calendar {
+    z-index: 9;
+    position: absolute;
+    top: 35px;
+    right: 0;
+    min-width: 290px;
+    height: auto;
+    max-width: 100%;
+    box-shadow: 0px 16px 50px 0px rgba(33, 33, 33, 0.25);
+    border-radius: 16px;
+    background: #fff;
+    font-family: "Pretendard Variable", sans-serif;
+    line-height: 1rem;
+    border: none;
+    padding: 15px;
+  }
+  .react-calendar__navigation {
+    display: flex;
+    height: auto;
+    margin: 10px 0;
+  }
+  .react-calendar__navigation button:disabled {
+    background-color: #fff;
+  }
+  .react-calendar abbr {
+    text-decoration-line: blink;
+  }
+  .react-calendar__tile--now {
+    background: #fff;
+  }
+  .react-calendar__tile:disabled {
+    background-color: #fff;
+    color: #bdbdbd;
+  }
+  .react-calendar__tile:disabled abbr {
+    color: #bdbdbd;
+  }
+  .react-calendar button:enabled {
+    width: fit-content;
+    height: inherit;
+    border-radius: 100%;
+  }
+  .react-calendar__tile--active {
+    background-color: #000;
+    color: #fff;
+  }
+  .react-calendar__tile--active abbr {
+    color: #fff;
+  }
+  .react-calendar__tile:enabled:hover {
+    background-color: #fafafa;
+  }
+  .react-calendar__tile:enabled:focus {
+    background-color: #eeeeee;
+  }
+  .react-calendar__tile--active:enabled:hover,
+  .react-calendar__tile--active:enabled:focus {
+    background-color: #000;
+    color: #fff;
+  }
+
+  .react-calendar__navigation__label {
+    flex-grow: inherit !important;
+    padding-left: 10px;
+    color: #000;
+    font-size: 1rem;
+    font-weight: 700;
+  }
+  .react-calendar__navigation .react-calendar__navigation__arrow {
+    display: none;
   }
 `;
 
