4f9ba00cc19375cf259bee5152fc965a73488795
hoyyChoi <110888511+hoyyChoi@users.noreply.github.com>
2024-03-10 14:28:18 +0900

    feat : writeView edit

--- src/components/DetailPage/WriteView/index.tsx
+++ src/components/DetailPage/WriteView/index.tsx
@@ -1,15 +1,82 @@
-import { format } from "date-fns";
+import { useState } from "react";
 
+import { format } from "date-fns";
+import { useNavigate } from "react-router-dom";
+import { useSetRecoilState } from "recoil";
+
+import optionImg from "@/assets/DetailPage/option.svg";
+import pencilImg from "@/assets/DetailPage/pencil.svg";
 import { TitleSideBox } from "@/components/atom/TitleSideBox";
+import {
+  DetailModalState,
+  addSpecialQuestionState,
+  postEditWritingDataState,
+} from "@/recoil/atoms";
 import { communityContentProps } from "@/types";
 
 import { Container } from "./style";
 
-export const WriteView = ({ detailData }: { detailData: communityContentProps[] }) => {
+export const WriteView = ({
+  detailData,
+  nickName,
+}: {
+  detailData: communityContentProps[];
+  nickName: string;
+}) => {
+  const navigate = useNavigate();
+  const setDetailModal = useSetRecoilState(DetailModalState);
+  const setPostEditWritingData = useSetRecoilState(postEditWritingDataState);
+  const setIsClickArray = useSetRecoilState(addSpecialQuestionState);
+
   const arr = detailData?.filter((item) => item.category === "스페셜 질문");
+  const [isOptionBox, setIsOptionBox] = useState<boolean>(false);
+  const SpaceToEditWritePage = () => {
+    setDetailModal(false);
+    document.body.style.overflowY = "scroll";
+    setIsClickArray(
+      detailData.filter((item) => item.category === "스페셜 질문").map((obj) => obj.question_id)
+    );
+    setPostEditWritingData(
+      detailData.map((obj) => ({
+        userTempleteId: obj.user_templete_id,
+        category: obj.category,
+        question: obj.question,
+        question_id: obj.question_id,
+        content: obj.content,
+        visibility: obj.visibility === 1 ? true : false, // 예시로 true로 설정했지만 필요에 따라 다른 값으로 설정 가능
+      }))
+    );
+    navigate(`/editwriting/${format(detailData[0]?.created_at, "yyyy-MM-dd")}`);
+  };
+
   return (
     <Container>
-      <div className="date">{format(detailData[0]?.created_at, "M월 d일 회고")}</div>
+      <div className="top">
+        <div className="date">{format(detailData[0]?.created_at, "M월 d일 회고")}</div>
+        {nickName === detailData[0].nickname && (
+          <div
+            className="option"
+            onClick={() => setIsOptionBox(!isOptionBox)}
+          >
+            <img
+              src={optionImg}
+              alt=":"
+            />
+          </div>
+        )}
+        {isOptionBox && (
+          <div
+            className="editBox"
+            onClick={SpaceToEditWritePage}
+          >
+            <p>수정하기</p>
+            <img
+              src={pencilImg}
+              alt="."
+            />
+          </div>
+        )}
+      </div>
       {arr.length > 0 && (
         <div className="specialQuestion">
           <TitleSideBox type="special">스페셜 질문</TitleSideBox>
