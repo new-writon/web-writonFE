dcb74754480105bde4b787888d7b8bc00c05c3e1
hoyyChoi <110888511+hoyyChoi@users.noreply.github.com>
2024-03-10 14:29:53 +0900

    fix : dummyData

--- src/dummy/main.ts
+++ src/dummy/main.ts
@@ -1,3 +1,1 @@
-export const mainCalendarDummyData = [
-  { date: "2024-02-06T00:00:00.000Z", badge: "hello", length: 1 },
-];
+export const mainCalendarDummyData = [{ date: new Date(), badge: "hello", length: 1 }];
