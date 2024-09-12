cac76ace16e17f8968978c19ac88b058ac4bca0b
hoyyChoi <hoyeon8165@gmail.com>
2024-09-12 20:37:34 +0900

    fix : routing (suspense,lazy-loading)

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,23 +1,27 @@
-/* eslint-disable react-hooks/rules-of-hooks */
+import { Suspense, lazy } from "react";
+
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Layout from "@/Layout";
+import Loading from "@/components/Common/Loading";
 import { ModalProvider } from "@/components/Common/ModalProvider";
 import { ScrollToTop } from "@/components/Common/ScrollToTop";
 import { SnackBarProvider } from "@/components/Common/SnackBarProvider";
-import { KakaoCallback } from "@/pages/Authorization/KakaoCallback";
-import LoginPage from "@/pages/Authorization/LoginPage";
-import RegisterEmailPage from "@/pages/Authorization/RegisterEmailPage";
-import RegisterPage from "@/pages/Authorization/RegisterPage";
-import { CommunityPage } from "@/pages/CommunityPage/CommunityPage";
-import { DetailPage } from "@/pages/DetailPage/DetailPage";
-import { EditWritingPage } from "@/pages/EditWritingPage/EditWritingPage";
-import MainPage from "@/pages/MainPage/MainPage";
-import { MyPage } from "@/pages/MyPage/MyPage";
-import { MyPageMobile } from "@/pages/MyPage/MyPageMobile";
-import { NotificationPage } from "@/pages/NotificationPage/NotificationPage";
-import { OnboardingPage } from "@/pages/OnboardingPage/OnboardingPage";
-import { WritingPage } from "@/pages/WritingPage/WritingPage";
+
+// 동적 로딩을 위한 React.lazy 사용
+const KakaoCallback = lazy(() => import("@/pages/Authorization/KakaoCallback"));
+const LoginPage = lazy(() => import("@/pages/Authorization/LoginPage"));
+const RegisterEmailPage = lazy(() => import("@/pages/Authorization/RegisterEmailPage"));
+const RegisterPage = lazy(() => import("@/pages/Authorization/RegisterPage"));
+const CommunityPage = lazy(() => import("@/pages/CommunityPage/CommunityPage"));
+const DetailPage = lazy(() => import("@/pages/DetailPage/DetailPage"));
+const EditWritingPage = lazy(() => import("@/pages/EditWritingPage/EditWritingPage"));
+const MainPage = lazy(() => import("@/pages/MainPage/MainPage"));
+const MyPage = lazy(() => import("@/pages/MyPage/MyPage"));
+const MyPageMobile = lazy(() => import("@/pages/MyPage/MyPageMobile"));
+const NotificationPage = lazy(() => import("@/pages/NotificationPage/NotificationPage"));
+const OnboardingPage = lazy(() => import("@/pages/OnboardingPage/OnboardingPage"));
+const WritingPage = lazy(() => import("@/pages/WritingPage/WritingPage"));
 
 const router = () => {
   const PrivateRoute = () => {
@@ -32,69 +36,93 @@ const router = () => {
       <SnackBarProvider />
       <ModalProvider />
       <ScrollToTop />
-
-      <Routes>
-        <Route
-          path="/login"
-          element={<LoginPage />}
-        />
-        <Route
-          path="/register"
-          element={<RegisterPage />}
-        />
-        <Route
-          path="/registeremail"
-          element={<RegisterEmailPage />}
-        />
-        <Route
-          path="/auth/kakao/callback"
-          element={<KakaoCallback />}
-        />
-        <Route
-          path="/onboarding"
-          element={<OnboardingPage />}
-        />
-        <Route
-          path="/mypageMobile"
-          element={<MyPageMobile />}
-        />
-        <Route
-          path="/notificationMobile"
-          element={<NotificationPage />}
-        />
-        <Route element={<Layout />}>
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          {/* 기본 레이아웃 */}
           <Route
             path="/"
-            element={<PrivateRoute />}
+            element={<Layout />}
+          >
+            {/* 메인 페이지 */}
+            <Route
+              index
+              element={<PrivateRoute />}
+            />
+
+            {/* Writing 관련 페이지 */}
+            <Route
+              path="writing/:date"
+              element={<WritingPage />}
+            />
+            <Route
+              path="editwriting/:date"
+              element={<EditWritingPage />}
+            />
+
+            {/* 커뮤니티 페이지 */}
+            <Route
+              path="community"
+              element={<CommunityPage />}
+            />
+
+            {/* 상세 페이지 */}
+            <Route
+              path="detail/:templateId"
+              element={<DetailPage />}
+            />
+
+            {/* 마이 페이지 */}
+            <Route
+              path="mypage"
+              element={<MyPage />}
+            />
+          </Route>
+
+          {/* 로그인 및 회원가입 관련 페이지 */}
+          <Route
+            path="login"
+            element={<LoginPage />}
           />
           <Route
-            path="/writing/:date"
-            element={<WritingPage />}
+            path="register"
+            element={<RegisterPage />}
           />
           <Route
-            path="/editwriting/:date"
-            element={<EditWritingPage />}
+            path="registeremail"
+            element={<RegisterEmailPage />}
           />
           <Route
-            path="/community"
-            element={<CommunityPage />}
+            path="auth/kakao/callback"
+            element={<KakaoCallback />}
+          />
+
+          {/* 온보딩 페이지 */}
+          <Route
+            path="onboarding"
+            element={<OnboardingPage />}
+          />
+
+          {/* 모바일 페이지들 */}
+          <Route
+            path="mypageMobile"
+            element={<MyPageMobile />}
           />
           <Route
-            path="/detail/:templateId"
+            path="notificationMobile"
+            element={<NotificationPage />}
+          />
+          <Route
+            path="notificationMobile/detail/:templateId"
             element={<DetailPage />}
           />
 
-          {/* 알림창에서 디테일 들어가기 (모바일) */}
+          {/* Not Found 페이지 처리 */}
           <Route
-            path="/notificationMobile/detail/:templateId"
-            element={<DetailPage />}
+            path="*"
+            element={<div>Not Found</div>}
           />
-          <Route
-            path="/mypage"
-            element={<MyPage />}
-          />
-        </Route>
-      </Routes>
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 };
