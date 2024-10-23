37b6853b2fbbb291e235315a5a5a4bb14f3eaf5a
hoyyChoi <hoyeon8165@gmail.com>
2024-10-23 20:22:40 +0900

    fix : serviceworker logic (async/await)

--- src/utils/notification.ts
+++ src/utils/notification.ts
@@ -1,12 +1,8 @@
-export function registerServiceWorker() {
-  navigator.serviceWorker
-    .register("firebase-messaging-sw.js")
-    .then(function (registration) {
-      console.log("Service Worker 등록 성공:", registration);
-      alert(`Service Worker 등록 성공:, ${registration}`);
-    })
-    .catch(function (error) {
-      console.log("Service Worker 등록 실패:", error);
-      alert(`Service Worker 등록 실패:, ${error}`);
-    });
+export async function registerServiceWorker() {
+  try {
+    const registration = await navigator.serviceWorker.register("firebase-messaging-sw.js");
+    console.log("Service Worker 등록 성공:", registration);
+  } catch (error) {
+    console.log("Service Worker 등록 실패:", error);
+  }
 }
