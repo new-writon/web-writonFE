e6f1761bfbfb925d5cf6c2a9a604a092f182b111
hoyyChoi <hoyeon8165@gmail.com>
2025-06-15 16:42:54 +0900

    feat: 더미데이터 추가

--- src/dummy/main.ts
+++ src/dummy/main.ts
@@ -1,1 +1,238 @@
 export const mainCalendarDummyData = [{ date: new Date(), badge: "hello", length: 1 }];
+
+export const getBasicQuestionData = [
+  {
+    questionId: 83,
+    question: "오늘의 업무는 무엇인가요? ",
+  },
+  {
+    questionId: 84,
+    question: "오늘의 잘한 점은 무엇인가요?",
+  },
+  {
+    questionId: 85,
+    question: "오늘의 개선 점은 무엇인가요?",
+  },
+  {
+    questionId: 86,
+    question: "오늘의 배운 점은 무엇인가요?",
+  },
+];
+
+export const getSpecialQuestionData = [
+  {
+    questionId: 95,
+    question:
+      "본인이 현재 하고 있는 인턴 생활을 통해서, (1) 꼭 얻어가고싶은 것, (2) 굳이 얻어가지 않아도 될 것을 묻는 다면 어떻게 답하실건가요?",
+    keyword: "가치",
+  },
+  {
+    questionId: 96,
+    question: "자신의 능력 밖을 벗어난 업무가 주어진다면 어떻게 하겠습니까?",
+    keyword: "가치",
+  },
+  {
+    questionId: 98,
+    question: "직무를 바꿀 수 있는 기회가 주어진다면 바꿀 것인가요? 바꾼다면 어디로?",
+    keyword: "가치",
+  },
+  {
+    questionId: 100,
+    question: "나만의 스트레스 해소법은 무엇인가요?",
+    keyword: "가치",
+  },
+  {
+    questionId: 101,
+    question: "본인은 회사에서 스트레스를 받는 편인가요?",
+    keyword: "가치",
+  },
+  {
+    questionId: 102,
+    question: "본인에게 ‘워라밸’이란 무엇인가요? ",
+    keyword: "가치",
+  },
+  {
+    questionId: 103,
+    question: "나는 언제 은퇴하여, 은퇴 후 어떤 삶을 살고 싶나요?",
+    keyword: "가치",
+  },
+  {
+    questionId: 105,
+    question: "당신이 상사라면, 당신을 채용할 것인가요? 이유는?!",
+    keyword: "가치",
+  },
+  {
+    questionId: 109,
+    question: "‘좋은 동료’란 무엇이라고 생각하나요?",
+    keyword: "가치",
+  },
+  {
+    questionId: 110,
+    question: "‘좋은 리더’란 무엇이라고 생각하나요?",
+    keyword: "가치",
+  },
+  {
+    questionId: 113,
+    question: "상사와 의견이 다를 시 어떻게 하나요?",
+    keyword: "가치",
+  },
+  {
+    questionId: 115,
+    question: "경영이란 무엇이라고 생각하나요?",
+    keyword: "가치",
+  },
+  {
+    questionId: 116,
+    question: "나에게 성공이란 무엇인가요?",
+    keyword: "가치",
+  },
+  {
+    questionId: 125,
+    question: "내가 좋아하지 않는 동료는 어떤 사람인가요?",
+    keyword: "가치",
+  },
+  {
+    questionId: 89,
+    question:
+      '내가 우리 회사에서 "가장 힘들다"고 생각하는 것이 무엇인가요? 이를 기록해보고, 내가 회사에서 일할 때 (혹은 조직에서 일할 때) 무엇이 힘든 사람인지, 어떤 요소가 나를 힘들게 하는 것들인지 생각해보세요!',
+    keyword: "경험",
+  },
+  {
+    questionId: 92,
+    question:
+      "인턴을 하며, 예기치 못한 상황&난감했던 상황 등이 있다면, (1) 어떤 상황이었는지, (2) 어떻게 대처하였는지 작성해보세요!",
+    keyword: "경험",
+  },
+  {
+    questionId: 104,
+    question: "난감했거나 거절하기 어려운 상황?!",
+    keyword: "경험",
+  },
+  {
+    questionId: 118,
+    question: "가장 최선을 다했던 경험이 무엇인가요?",
+    keyword: "경험",
+  },
+  {
+    questionId: 119,
+    question: "다른 사람과 협업, 협력했던 경험을 설명해주세요",
+    keyword: "경험",
+  },
+  {
+    questionId: 120,
+    question: "갈등을 겪고 해결해본 경험을 설명해주세요",
+    keyword: "경험",
+  },
+  {
+    questionId: 121,
+    question: "끈질기게 노력하고 도전해본 경험을 설명해주세요",
+    keyword: "경험",
+  },
+  {
+    questionId: 122,
+    question: "창의성을 발휘해본 경험을 설명해주세요",
+    keyword: "경험",
+  },
+  {
+    questionId: 123,
+    question: "실패해본 경험을 설명해주세요",
+    keyword: "경험",
+  },
+  {
+    questionId: 94,
+    question: "앞으로 6-12개월 동안 달성하고 싶은 커리어 목표는 무엇인가요?",
+    keyword: "목표",
+  },
+  {
+    questionId: 88,
+    question:
+      '문화우리 회사의 "문화"는 어떤 것 같나요? 대표적으로 자랑할 수 있는 조직문화나 내가 좋아하는 사소한 문화 등 인턴을 하고있는 회사의 문화를 소개해주세요! 나는 그러한 문화가 좋은지/별로인지, 앞으로도 계속 이러한 문화에 몸담고 싶은지 등을 작성해보세요',
+    keyword: "문화",
+  },
+  {
+    questionId: 97,
+    question: "내가 지금 일하고 있는 곳은 업무 강도가 센 편인가요? 약한 편인가요?",
+    keyword: "문화",
+  },
+  {
+    questionId: 99,
+    question: "우리 회사의 비즈니스 모델은 무엇인가요? 무엇으로 매출을 창출하고 있나요?",
+    keyword: "문화",
+  },
+  {
+    questionId: 124,
+    question: "지금 속해있는 조직에서 무언가 바꿀 수 있다면 무엇을 바꾸시겠습니까?",
+    keyword: "문화",
+  },
+  {
+    questionId: 90,
+    question: "스스로를 '팔로워형'이라고 생각하시나요? '리더형'이라고 생각하시나요? 왜 그런가요!?",
+    keyword: "성향",
+  },
+  {
+    questionId: 117,
+    question: "주변 사람들은 나를 뭐라고 평가하나요?",
+    keyword: "성향",
+  },
+  {
+    questionId: 87,
+    question:
+      '오늘 한 업무 중, 다른 사람과의 "커뮤니케이션" 역량이나 "협업" 능력을 보여줄 수 있는 업무가 있었나요?',
+    keyword: "역량",
+  },
+  {
+    questionId: 91,
+    question:
+      '인턴 생활을 돌아보며, 나의 "커뮤니케이션 역량" 이나 "팀워크 역량" 혹은 "책임감 역량"을 보여줄 수 있는 경험이 있다면 기록해주세요!',
+    keyword: "역량",
+  },
+  {
+    questionId: 93,
+    question:
+      '이번주 혹은 인턴 생활을 통틀어 돌아보며, 나의 "추진력"이나 "기획력" 혹은 "주도성" 역량을 보여줄 수 있는 경험이 있다면 공유해주세요! (해당 역량에 대해 나만의 정의를 내린 후, 그 역량을 잘 보여줄 수 경험을 적어보면 좋아요)',
+    keyword: "역량",
+  },
+  {
+    questionId: 106,
+    question:
+      "팀원들과의 결속력, 팀워크를 다지기 위해 나는 역할을 수행했나요? 앞으로 어떤 역량이 더 필요할까요?",
+    keyword: "역량",
+  },
+  {
+    questionId: 107,
+    question: "인턴십을 하면서 발견한 나의 가장 큰 장점/강점은?",
+    keyword: "역량",
+  },
+  {
+    questionId: 108,
+    question: "인턴십을 하면서 발견한 나의 가장 큰 약점/보완점은?",
+    keyword: "역량",
+  },
+  {
+    questionId: 111,
+    question: "업무 상 나의 가장 큰 장점은?",
+    keyword: "역량",
+  },
+  {
+    questionId: 112,
+    question: "업무 상 나의 가장 큰 단점은?",
+    keyword: "역량",
+  },
+  {
+    questionId: 114,
+    question: "우선순위를 어떻게 관리하나요?",
+    keyword: "역량",
+  },
+  {
+    questionId: 126,
+    question:
+      "그동안 진행한 업무를 돌아보며, 나의 ‘분석력’이나 ‘전략적 사고‘ 역량을 보여줄 수 있는 경험이 있다면 공유해주세요! (해당 역량에 대해 나만의 정의를 내린 후, 그 역량을 잘 보여줄 수 경험을 적어보면 좋아요)",
+    keyword: "역량",
+  },
+  {
+    questionId: 127,
+    question:
+      "그동안 진행한 업무를 돌아보며, 나의 ‘상황대처력’, ‘위기대처역량’을 보여줄 수 있는 경험이 있다면 공유해주세요! (해당 역량에 대해 나만의 정의를 내린 후, 그 역량을 잘 보여줄 수 경험을 적어보면 좋아요)",
+    keyword: "역량",
+  },
+];
